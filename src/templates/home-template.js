68d5f545e4701f864cf5f98cfa9a098e6dca9429
Osman Adem <112473033+OsmanAdem@users.noreply.github.com>
2024-01-21 02:34:43 +0100

    New Links

--- src/templates/home-template.js
+++ src/templates/home-template.js
@@ -3,10 +3,12 @@ import { GatsbyImage } from 'gatsby-plugin-image'
  
 const homeTemplate = (contentfulPage) => {
   return (
+    <>
     <main>
       <h2>{contentfulPage.title}</h2>
       {contentfulPage.image && <GatsbyImage alt={contentfulPage.title} image={contentfulPage.image.gatsbyImage}></GatsbyImage>}
       </main>
+    </>
   )
 }
  
